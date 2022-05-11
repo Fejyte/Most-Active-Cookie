ec5292ce910744a0fe397306fb4a011960e00dcd
Amam <oea120@ic.ac.uk>
2022-05-11 17:39:10 +0100

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Most-Active-Cookie
+
+This program  processes the log file and return the most active
+cookie for specified day (defined as teh cookie seen the most times during the day)
+
+This directory consists of the following files:
+    
+- most_active_cookie.py
+  - This is the script which contains the command line program. It consists of several function which 
+    receive information from the terminal and return the most active cookie(s)
+- test_most_active_cookie.py
+  - This contains various tests for the functions written in the "most active cookie file"
+- cookie_log.csv
+  - This csv file contains the cookie data given in the coding task exercise. It was used for testing the program
+- README.md
+
+
+The command line program is run using the following command:
+    
+- python most_active_cookie _filename_ -d _date_
+  - for example - python most_active_cookie cookie_log.csv -d 2018-12-09
+
+Tests can be run with the Pytest framework using the following command:
+-  pytest test_most_active_cookie.py
+
+
+Note:
+- Please ensure the file is a csv file
+- For testing, please ensure all aforementioned files are in the same directory
--- test_most_active_cookie.py
+++ test_most_active_cookie.py
@@ -1,22 +1,28 @@
 import pytest
-from most_active_cookie import read_cli
+import os
+from most_active_cookie import get_arguments
 from most_active_cookie import date_time_converter
 from most_active_cookie import read_cookie_data
 from most_active_cookie import get_most_used_cookie
 
 
-#What to test
-# Common Cases
-# Edge Case
-# Boundary Cases
-def test_read_cli():
-    assert True
-
-def test_date_time_converter():
+def test_date_time_converter_is_correct():
     assert date_time_converter("2018-12-09T14:19:00") == "2018-12-09"
 
-def test_get_most_used_cookies():
+
+def test_get_most_used_cookies_correctly_returns_single_cookie():
     assert get_most_used_cookie("cookie_log.csv", "2018-12-09") == ["AtY0laUfhglK3lC7"]
 
-def test_get_most_used_cookies():
-    assert get_most_used_cookie("cookie_log.csv", "2018-12-08") == ["AtY0laUfhglK3lC7"]
+
+def test_get_most_used_cookies_correctly_returns_multiple_cookies():
+    assert get_most_used_cookie("cookie_log.csv", "2018-12-08") == ["SAZuXPGUrfbcn5UA","4sMM2LxV07bPJzwf","fbcn5UAVanZf6UtG"]
+
+
+def test_read_cookie_data_returns_something():
+    assert read_cookie_data("cookie_log.csv")
+
+
+def test_get_arguments_returns_correct_values():
+    assert get_arguments()['filename'] == "test_most_active_cookie.py"
+
+
